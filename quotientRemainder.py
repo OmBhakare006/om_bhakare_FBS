07fce637a57a1782fe105abae9f189b88645a6a1
OmBhakare006 <ombhakare006@gmail.com>
2025-08-21 20:42:09 +0530

    Update quotientRemainder.py

--- quotientRemainder.py
+++ quotientRemainder.py
@@ -1,4 +1,4 @@
-#3.Program to find quotient and remainder of two numbers.?
+# Q8.Program to find quotient and remainder of two numbers.?
 num1 = int(input('enter the num 1 :  '))
 num2 = int(input('enter the num 2 :  '))
 
@@ -6,4 +6,5 @@ quot = num1 // num2
 print(f'qoutient is: {quot}')
 
 remai = num1 % num2
+
 print(f'remainder is: {remai}')
