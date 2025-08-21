bcf022fe314ee49055341179859923ad7a7b65ce
OmBhakare006 <ombhakare006@gmail.com>
2025-08-21 20:37:20 +0530

    Update areaOfRecṭ.py

--- areaOfRecṭ.py
+++ areaOfRecṭ.py
@@ -1,8 +1,9 @@
-# 2.WAP to calculate area of rectangle based on length and breadth ??
+# Q1.WAP to calculate area of rectangle based on length and breadth ??
 
 length = int(input('enter the length: '))
 breadth = int(input('enter the breadth: '))
 
 rect_area = length * breadth
+
 
 print(f'area of rectangle is : {rect_area}')
