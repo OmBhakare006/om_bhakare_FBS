76cbc858a4226275b83d19dfd7da9ad491038a99
OmBhakare006 <ombhakare006@gmail.com>
2025-08-21 20:38:49 +0530

    Update circumfeOfCir.py

--- circumfeOfCir.py
+++ circumfeOfCir.py
@@ -1,4 +1,4 @@
-#Q11.Find the area and circumference of circle.??
+#Q3.Find the area and circumference of circle.??
 
 r = int(input('enter the value: '))
 
@@ -7,3 +7,4 @@ circum = 2 * 3.14 * r
 area = 3.14 * r**2
 
 print(f'circumference of the circle is: {circum}\narea of the circle is: {area}')
+
