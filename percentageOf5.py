c3b8aff29e51ccf7e09ecab72a1ed0becfd8dae7
OmBhakare006 <ombhakare006@gmail.com>
2025-08-21 20:41:29 +0530

    Update percentageOf5.py

--- percentageOf5.py
+++ percentageOf5.py
@@ -1,4 +1,4 @@
-#1. WAP to calculate the percentage of student based on marks of any 5 subjects.??
+#6. WAP to calculate the percentage of student based on marks of any 5 subjects.??
 
 m1 = int(input('enter the marks of 1 : '))
 m2 = int(input('enter the marks of 2 : '))
@@ -9,5 +9,6 @@ m5 = int(input('enter the marks of 5 : '))
 gain_marks = m1 + m2 + m3 + m4 + m5
 
 perc = (gain_marks/500)*100
+
 
 print(f'percentage is : {perc}')
