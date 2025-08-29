95bb89a5dd1c93194fb56ba1806f55848c1c14f6
OmBhakare006 <ombhakare006@gmail.com>
2025-08-30 00:09:14 +0530

    Delete que 2.py

--- que 2.py
+++
@@ -1,7 +0,0 @@
-prin = int(input('enter the principle: '))
-rate = int(input('enter the rate: '))
-time = int(input('enter the time: '))
-
-s_i = (prin * rate * time)/100
-
-print(f'simple interest is: {s_i}')
