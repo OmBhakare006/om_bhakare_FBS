2a9941da2379843412c7504d2a4ca809afd8e9b4
OmBhakare006 <ombhakare006@gmail.com>
2025-08-30 00:09:25 +0530

    Delete que 3.py

--- que 3.py
+++
@@ -1,9 +0,0 @@
-dist = int(input('enter the distance in km:  '))
-
-#lets convert distance in km to meter and centimeter
-
-meter = dist * 1000
-print(meter)
-
-centimeter = dist * 100000
-print(centimeter)
