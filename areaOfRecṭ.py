4d56d55db3ebb712c391876fc7dab7724ce68ac9
OmBhakare006 <ombhakare006@gmail.com>
2025-08-30 00:06:05 +0530

    Delete areaOfRecṭ.py

--- areaOfRecṭ.py
+++
@@ -1,9 +0,0 @@
-# Q1.WAP to calculate area of rectangle based on length and breadth ??
-
-length = int(input('enter the length: '))
-breadth = int(input('enter the breadth: '))
-
-rect_area = length * breadth
-
-
-print(f'area of rectangle is : {rect_area}')
