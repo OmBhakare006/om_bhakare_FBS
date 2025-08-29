d37f22f2c4883a8823e7f9b8cd6990d1f46b3916
OmBhakare006 <ombhakare006@gmail.com>
2025-08-29 21:50:19 +0530

    Add files via upload
    This commit contains the complete total 4 question of solutions for the recent test.  
    - Implemented solutions in Python, focusing on clear logic, readability, and step-by-step explanation.  
    - Each question is solved separately.  
    - Added comments in the code to explain the approach and reasoning behind each solution.  
    - These solutions will serve as a reference for revision, practice, and future improvements.  
    
    By uploading these files, the GitHub repository now contains all the required answers for the test, making it easier to track progress and share with others if needed.

---
+++ que 2.py
@@ -0,0 +1,7 @@
+prin = int(input('enter the principle: '))
+rate = int(input('enter the rate: '))
+time = int(input('enter the time: '))
+
+s_i = (prin * rate * time)/100
+
+print(f'simple interest is: {s_i}')
---
+++ que 3.py
@@ -0,0 +1,9 @@
+dist = int(input('enter the distance in km:  '))
+
+#lets convert distance in km to meter and centimeter
+
+meter = dist * 1000
+print(meter)
+
+centimeter = dist * 100000
+print(centimeter)
