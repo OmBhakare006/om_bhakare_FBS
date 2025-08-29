aa092060be73280bbc06f4ec2611de82158aac1a
OmBhakare006 <ombhakare006@gmail.com>
2025-08-30 00:06:20 +0530

    Delete areaOfTriangle.py

--- areaOfTriangle.py
+++
@@ -1,9 +0,0 @@
-#Q2.Write a program to enter base and height of a triangle and find its area.??
-
-height = float(input('enter the height: '))
-base = float(input('enter the base:  '))
-
-area = 1/2*(height*base)
-
-
-print(f'area of triangle is : {area}. ')
